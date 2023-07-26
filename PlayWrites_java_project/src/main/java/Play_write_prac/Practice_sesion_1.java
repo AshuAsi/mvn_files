349c7e8637528cced99682aa8deb6a62b4000c99
AshuAsi <138884056+AshuAsi@users.noreply.github.com>
2023-07-26 17:11:38 +0530

    commit

--- PlayWrites_java_project/src/main/java/Play_write_prac/Practice_sesion_1.java
+++ PlayWrites_java_project/src/main/java/Play_write_prac/Practice_sesion_1.java
@@ -11,19 +11,19 @@ public class Practice_sesion_1 {
 	public static void main(String[] args) 
 	{
 		Playwright pw = Playwright.create();//(obj create)
-		Browser bw=pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)); 
+		Browser bw=pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)); //webdriver
 		Page p=bw.newPage();
 		
 		p.navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		Locator user=p.locator("//input[@name='username']");
 		user.type("Admin");
 		
-			Locator password=p.locator("//input[@name='password']");//webelement
+			Locator password=p.locator("//input[@name='password']");//webelement 
 		password.type("admin123");//typemeans sendkeys
 		
 		Locator login=p.locator("//button[@type='submit']");
+	
 		login.click();
-		
 
 	}
 
