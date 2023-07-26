e14d65d1fe78844645a4311aa8076cf6eb523d26
AshuAsi <138884056+AshuAsi@users.noreply.github.com>
2023-07-26 12:50:15 +0530

    playwright_basics_comiit

---
+++ PlayWrites_java_project/src/main/java/Play_write_prac/Prac_seassion_2.java
@@ -0,0 +1,10 @@
+package Play_write_prac;
+
+public class Prac_seassion_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ PlayWrites_java_project/src/main/java/Play_write_prac/Practice_sesion_1.java
@@ -0,0 +1,30 @@
+package Play_write_prac;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Practice_sesion_1 {
+
+	public static void main(String[] args) 
+	{
+		Playwright pw = Playwright.create();//(obj create)
+		Browser bw=pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)); 
+		Page p=bw.newPage();
+		
+		p.navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Locator user=p.locator("//input[@name='username']");
+		user.type("Admin");
+		
+			Locator password=p.locator("//input[@name='password']");//webelement
+		password.type("admin123");//typemeans sendkeys
+		
+		Locator login=p.locator("//button[@type='submit']");
+		login.click();
+		
+
+	}
+
+}
